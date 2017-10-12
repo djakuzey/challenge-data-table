7654a4c0ad2faf1e5c553ad53e54cd3b19becebb
djakuzey <balyko.peaceit@gmail.com>
2017-10-12 10:44:42 +0300

    challenge data table

--- report.jsx
+++ report.jsx
@@ -4,8 +4,64 @@ var createReactClass = require('create-react-class')
 
 var rows = require('./data.json')
 
+var dimensions = [
+  { value: 'date', title: 'Date' },
+  { value: 'host', title: 'Host' }
+]
+
+var increaseRowCount = function(row, type, memory) {
+  return row.type === type
+    ? (memory[type + 's'] || 0) + 1
+    : memory[type + 's']
+}
+
+var reduce = function(row, memory) {
+  memory.impressions = increaseRowCount(row, 'impression', memory)
+  memory.displays = increaseRowCount(row, 'display', memory)
+  memory.loads = increaseRowCount(row, 'load', memory)
+
+  return memory
+}
+
+var calculations = [
+  {
+    title: 'Impressions',
+    value: 'impressions'
+  },
+  {
+    title: 'Loads',
+    value: 'loads'
+  },
+  {
+    title: 'Displays',
+    value: 'displays'
+  },
+  {
+    title: 'Load Rate',
+    value: function (row) {
+      return ((row.loads / row.impressions) * 100).toFixed(1) + '%'
+    }
+  },
+  {
+    title: 'Display Rate',
+    value: function (row) {
+      return ((row.displays / row.loads) * 100).toFixed(1) + '%'
+    }
+  }
+];
+
 module.exports = createReactClass({
   render () {
-    return <div>Report</div>
+    return (
+      <div>
+        <ReactPivot
+          rows={rows}
+          dimensions={dimensions}
+          calculations={calculations}
+          reduce={reduce}
+          activeDimensions={['Date', 'Host']}
+        />
+      </div>
+    )
   }
 })
